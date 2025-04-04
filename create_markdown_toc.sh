1ca2c11530369fd3bf32b9382dc63cf369848fd8
Simon-Brandt <simon.brandt@uni-greifswald.de>
2025-04-04 16:48:52 +0200

    Added ability to create more than one TOC, including TOCs for single sections

--- create_markdown_toc.sh
+++ create_markdown_toc.sh
@@ -6,12 +6,12 @@
 
 # Usage:
 # bash create_markdown_toc.sh [--help | --usage | --version]
-#                             [--add-title]
+#                             [--add-titles]
 #                             [--exclude-headers=HEADERS...]
 #                             [--exclude-levels=LEVELS...]
 #                             [--in-place]
 #                             [--out-file=FILE]
-#                             [--title=TITLE]
+#                             [--titles=TITLES]
 #                             input_file
 
 # Purpose: Extract Markdown headings from a file and convert them into a
@@ -72,8 +72,8 @@ function header_to_link() {
 declare in_file
 declare out_file
 declare in_place
-declare add_title
-declare title
+declare add_titles
+declare -a toc_titles
 declare -a excluded_headers
 declare -a excluded_levels
 
@@ -83,8 +83,8 @@ args=(
     "in_file          |            |                 | input_file |                   |         | file |      1 | Positional arguments |       | the input file from which to get the headers    "
     "out_file         | o          | out-file        | FILE       | ''                |         | file |      1 | Options              |       | the output file to write the TOC to             "
     "in_place         | i          | in-place        |            | false             |         | bool |      0 | Options              |       | act in-place, writing the TOC to the input file "
-    "add_title        | a          | add-title       |            | true              |         | bool |      0 | Options              |       | add a title to the TOC                          "
-    "title            | t          | title           |            | Table of contents |         | str  |      1 | Options              |       | the name of the title to add to the TOC         "
+    "add_titles       | a          | add-titles      |            | true              |         | bool |      0 | Options              |       | add a title to each TOC                         "
+    "toc_titles       | t          | titles          |            | Table of contents |         | str  |      + | Options              |       | the TOC titles to add to the TOCs               "
     "excluded_headers | e          | exclude-headers | HEADERS    | ''                |         | str  |      + | Options              |       | comma-separated list of header names to exclude "
     "excluded_levels  | l          | exclude-levels  | LEVELS     | 0                 |         | uint |      + | Options              |       | comma-separated list of header levels to exclude"
 )
@@ -132,11 +132,19 @@ done
 shopt -s extglob
 
 headers=( )
+header_levels=( )
+header_line_indices=( )
+
 is_fenced_code_block_backtick=false
 is_fenced_code_block_tilde=false
 is_indented_code_block=false
 is_toc_block=false
+
 toc_level=1
+toc_levels=( )
+toc_starts=( )
+toc_ends=( )
+
 prev_line=""
 
 mapfile -t lines < "${in_file}"
@@ -165,7 +173,7 @@ for i in "${!lines[@]}"; do
         # end it by the </toc> comment.
         if [[ "${line}" == "<!-- </toc> -->" ]]; then
             is_toc_block=false
-            toc_end="${i}"
+            toc_ends+=("${i}")
         fi
     elif [[ "${line}" == "\`\`\`"* ]]; then
         # The line starts a fenced code block using backticks.
@@ -180,129 +188,160 @@ for i in "${!lines[@]}"; do
         # The line denotes the start of the table of contents for later
         # in-place addition.
         is_toc_block=true
-        toc_start="${i}"
+        toc_starts+=("${i}")
+        toc_levels+=("${toc_level}")
     elif [[ "${line}" == *( )+(\#)+( )* ]]; then
         # The line is a header, starting with hashmarks, followed by at
-        # least one space.  Count the hashmarks.
+        # least one space.  Count the hashmarks, after having removed
+        # the leading spaces.
         headers+=("${line}")
+        header_line_indices+=("${i}")
 
-        if [[ ! -v toc_start ]]; then
-            toc_level=0
-            while [[ "${line:toc_level:1}" == "#" ]]; do
-                (( toc_level++ ))
-            done
-        fi
+        header_level=0
+        line="${line##+( )}"
+        while [[ "${line:header_level:1}" == "#" ]]; do
+            (( header_level++ ))
+        done
+        header_levels+=("${header_level}")
+
+        (( toc_level = header_level + 1 ))
     elif [[ "${line}" == *( )+(=) && "${prev_line}" == *( )[^=\ ]* ]]; then
         # The line consists of equals signs, but the previous line
         # doesn't start with an equals sign and thus is a first-level
         # header.
         headers+=("# ${prev_line}")
-        if [[ ! -v toc_start ]]; then
-            toc_level=1
-        fi
+        header_line_indices+=("${i}")
+
+        header_level=1
+        header_levels+=("${header_level}")
+
+        (( toc_level = header_level + 1 ))
     elif [[ "${line}" == *( )+(-) && "${prev_line}" == *( )[^-\ ]* ]]; then
         # The line consists of hyphens, but the previous line doesn't
         # start with a hyphen and thus is a second-level header.
         headers+=("## ${prev_line}")
+        header_line_indices+=("${i}")
 
-        if [[ ! -v toc_start ]]; then
-            toc_level=2
-        fi
+        header_level=2
+        header_levels+=("${header_level}")
+
+        (( toc_level = header_level + 1 ))
     fi
     prev_line="${line}"
 done
 
-# Possibly, set the table of contents' header, including a trailing
-# blank line (as empty string in the array).
-if [[ "${add_title}" == true ]]; then
-    printf -v header '%*s' "$((toc_level + 1))" ""
-    header="${header// /#}"
-    header+=" ${title}"
-    headers=("${header}" "${headers[@]}")
-    toc_lines=("${header}" "")
-else
-    toc_lines=( )
-fi
-
-# Convert the headers to valid hyperlinks.
-declare -A links
-for header in "${headers[@]}"; do
-    # Count the leading hashmarks in the header.  If the header level
-    # shall be excluded, skip it.
-    level=0
-    while [[ "${header:level:1}" == "#" ]]; do
-        (( level++ ))
-    done
-
-    for excluded_level in "${excluded_levels[@]}"; do
-        if [[ "${level}" == "${excluded_level}" ]]; then
-            continue 2
+# Create the tables of contents.
+tocs=( )
+for i in "${!toc_levels[@]}"; do
+    # Get all headers with the same or higher level, starting from the
+    # current table of content's end line index.
+    toc_level="${toc_levels[i]}"
+    toc_end="${toc_ends[i]}"
+    toc_headers=( )
+    toc_header_levels=( )
+    for j in "${!headers[@]}"; do
+        if (( "${header_line_indices[j]}" > toc_end )); then
+            if (( "${header_levels[j]}" < toc_level )); then
+                break
+            fi
+            toc_headers+=("${headers[j]}")
+            toc_header_levels+=("${header_levels[j]}")
         fi
     done
 
-    # Create the required list item indentation per header level.
-    for i in "${!included_levels[@]}"; do
-        if [[ "${level}" == "${included_levels[i]}" ]]; then
-            break
-        fi
-    done
+    # Possibly, add the current table of contents' header to the array.
+    # The actual header is added afterwards, such that the indentation
+    # of the list items in the table of contents can be correctly
+    # computed, without interfering with the header.
+    if [[ "${add_titles}" == true ]]; then
+        printf -v toc_header '%*s' "${toc_level}" ""
+        toc_header="${toc_header// /#} ${toc_titles[j]:-"${toc_titles[0]}"}"
+        toc_headers=("${toc_header}" "${toc_headers[@]}")
 
-    (( count = i * 2 ))
-    printf -v indentation '%*s' "${count}" ""
-    indentation="${indentation}- "
-
-    # Remove the leading and trailing spaces, and the leading hashmarks
-    # and spaces following them from the title.
-    title="${header}"
-    title="${title##+( )}"  # Remove leading spaces.
-    title="${title##+(#)}"  # Remove leading hashmarks.
-    title="${title##+( )}"  # Remove leading spaces.
-    title="${title%%+( )}"  # Remove trailing spaces.
-
-    # If the header name shall be excluded, skip it.
-    for excluded_header in "${excluded_headers[@]}"; do
-        if [[ "${title}" == "${excluded_header}" ]]; then
-            continue 2
-        fi
-    done
-
-    # Convert the header's characters to create a valid link.
-    link="${header}"
-    link="${link//[^[:alnum:] _-]/}"  # Remove any punctuation but "_" and "-".
-    link="${link##+( )}"              # Remove leading spaces.
-    link="${link%%+( )}"              # Remove trailing spaces.
-    link="${link// /-}"               # Replace spaces with hyphens.
-    link="${link@L}"                  # Make all characters lowercase.
-
-    # Check if the link is unique, else, append an integer.
-    if [[ -v links["${link}"] ]]; then
-        (( links[${link}]++ ))
-        link="${link}-${links[${link}]}"
-    else
-        links[${link}]=0
+        toc_header_levels=("${toc_level}" "${toc_header_levels[@]}")
     fi
 
-    # Add the resultant line to the table of contents.
-    toc_lines+=("${indentation}[${title}](#${link})")
+    # Create the current table of contents, converting all headers
+    # belonging to it to valid hyperlinks.
+    toc_lines=( )
+    declare -A links
+    for j in "${!toc_headers[@]}"; do
+        # Get the header level.  If it shall be excluded, skip it.
+        for excluded_level in "${excluded_levels[@]}"; do
+            if [[ "${toc_header_levels[j]}" == "${excluded_level}" ]]; then
+                continue 2
+            fi
+        done
+
+        # Convert the header to a title suitable for the table of
+        # contents.
+        header="${toc_headers[j]}"
+        header_to_title "${header}"
+
+        # If the header name shall be excluded, skip it.
+        for excluded_header in "${excluded_headers[@]}"; do
+            if [[ "${title}" == "${excluded_header}" ]]; then
+                continue 2
+            fi
+        done
+
+        # Create the required list item indentation per header level.
+        (( count = "${toc_header_levels[j]}" * 2 ))
+        printf -v indentation '%*s' "${count}" ""
+
+        # Convert the header's characters to create a valid link.
+        header_to_link "${header}"
+
+        # Add the resultant line to the table of contents.
+        toc_line="${indentation}- [${title}](#${link})"
+        toc_lines+=("${toc_line}")
+    done
+    unset links
+
+    # Get the common indentation depth of all table of contents' lines,
+    # and strip this.
+    common_indentation=12  # Maximum possible indentation for header level h6.
+    for toc_line in "${toc_lines[@]}"; do
+        stripped_line="${toc_line##+( )}"
+        (( count = "${#toc_line}" - "${#stripped_line}" ))
+
+        if (( count < common_indentation )); then
+            common_indentation="${count}"
+        fi
+    done
+
+    printf -v indentation '%*s' "${common_indentation}" ""
+
+    for j in "${!toc_lines[@]}"; do
+        toc_lines[j]="${toc_lines[j]##"${indentation}"}"
+    done
+
+    # Possibly, add now the current table of contents' header, including
+    # a trailing blank line (using an empty string in the array).
+    if [[ "${add_titles}" == true ]]; then
+        toc_lines=("${toc_header}" "" "${toc_lines[@]}")
+    fi
+
+    # Join the table of contents lines by newline characters and append
+    # it to the previous tables of contents.
+    tocs+=("$(printf '%s\n' "${toc_lines[@]}")")
 done
 
-# Join the table of contents lines by newline characters.
-toc_lines=("$(printf '%s\n' "${toc_lines[@]}")")
-
-if [[ "${in_place}" == true && -v toc_start ]]; then
-    # Replace the table of contents lines in the Markdown file.  Add
-    # the new table of contents between the retrieved start and end
-    # lines, thereby replacing their contents (i.e., the previous table
+if [[ "${in_place}" == true ]] && (( "${#tocs[@]}" > 0 )); then
+    # Replace the tables of contents' lines in the Markdown file.  Add
+    # the new tables of contents between the retrieved start and end
+    # lines, thereby replacing their contents (i.e., the previous tables
     # of contents).  Finally, join the (now sparse) array of lines and
     # write it back to the input file.
-    if [[ -n "${toc_lines[0]}" ]]; then
-        lines[toc_start]+=$'\n'
-        lines[toc_start]+="${toc_lines[0]}"
-    fi
+    for i in "${!tocs[@]}"; do
+        lines[toc_starts[i]]+=$'\n'
+        lines[toc_starts[i]]+="${tocs[i]}"
 
-    for (( i = toc_start + 1; i < toc_end; i++ )); do
-        unset 'lines[i]'
+        for (( j = "${toc_starts[i]}" + 1; j < "${toc_ends[i]}"; j++ )); do
+            unset 'lines[j]'
+        done
     done
+
     printf '%s\n' "${lines[@]}" > "${in_file}"
 else
     # Write the lines to the output file.
